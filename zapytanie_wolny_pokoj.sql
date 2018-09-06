e8d72b80662d8baadc3c1fb7b451b2b74deccde8
wwiktorowicz <43016538+wwiktorowicz@users.noreply.github.com>
2018-09-06 21:05:48 +0200

    Update zapytanie_wolny_pokoj.sql

--- zapytanie_wolny_pokoj.sql
+++ zapytanie_wolny_pokoj.sql
@@ -1,4 +1,4 @@
-	select  Hotel.nazwa, Hotel.adres_miasto, Hotel.idHotel, WidokZOkna.Widok, Pokoj.Cena, Pokoj.nrPokoju
+	/*select  Hotel.nazwa, Hotel.adres_miasto, Hotel.idHotel, WidokZOkna.Widok, Pokoj.Cena, Pokoj.nrPokoju
         from Hotel 
         inner join Pietro on Hotel.idHotel = Pietro.Hotel_idHotel 
         join Pokoj on Pokoj.Pietro_idPietro = Pietro.idPietro
@@ -9,5 +9,24 @@
 	and $pers_num <= StandardPokoju.maxLiczbaOsob 
         and Hotel.lokalizacja = '$place'
         and ((Zamowienie.data_od >= '$arr_date' and Zamowienie.data_od >= '$dep_date')
-        or (Zamowienie.data_do <= '$arr_date' and Zamowienie.data_do <= '$dep_date'));
+        or (Zamowienie.data_do <= '$arr_date' and Zamowienie.data_do <= '$dep_date'));*/
+	
+	select Hotel.nazwa, Hotel.adres_miasto, Hotel.idHotel,  Pokoj.Cena, Pokoj.nrPokoju
+	from Hotel 
+	join Pietro on Hotel.idHotel = Pietro.Hotel_idHotel 
+	join Pokoj on Pokoj.Pietro_idPietro = Pietro.idPietro
+	join StandardPokoju on Pokoj.StandardPokoju_idStandardPokoju = StandardPokoju.idStandardPokoju
+	join Zamowienie on Pokoj.nrPokoju = Zamowienie.Pokoj_nrPokoju
+	where 
+		StandardPokoju.StandardPokoju = 'Standard'
+		and standardpokoju.maxLiczbaOsob <= 5
+		and hotel.lokalizacja = 'Mazury' 
+		and Pokoj.nrPokoju not in  (select Pokoj.nrPokoju 
+			from Hotel 
+			inner join Pietro on Hotel.idHotel = Pietro.Hotel_idHotel 
+			join Pokoj on Pokoj.Pietro_idPietro = Pietro.idPietro
+			join StandardPokoju on Pokoj.StandardPokoju_idStandardPokoju = StandardPokoju.idStandardPokoju
+			join Zamowienie on Pokoj.nrPokoju = Zamowienie.Pokoj_nrPokoju
+                            where (Zamowienie.data_od <= '2018-08-15' and Zamowienie.data_do >= '2018-08-20'))
+group by Pokoj.nrPokoju;
 
